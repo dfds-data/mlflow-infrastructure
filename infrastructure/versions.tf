d603e4256888c89749bb165d1ebb4c3d5a1d2e3c
mamor <mamor@dfds.com>
2022-03-15 14:00:44 +0100

    Remove unused random provider

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1"
-    }
   }
   required_version = ">= 1.0.0"
 }
