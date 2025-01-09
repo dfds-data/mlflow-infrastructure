99f48dd41ae271b611fa774b8b135e82b3ed5959
pehan <pehan@dfds.com>
2025-01-09 09:29:11 +0100

    fix: Remove conflicting S3 bucket ACL resource from Terraform configuration
    If “Bucket owner enforced” is active for your S3 bucket, ACLs are effectively disabled. Attempting to set them via Terraform (aws_s3_bucket_acl) will fail with AccessControlListNotSupported. See https://github.com/hashicorp/terraform-provider-aws/issues/34079

--- mlflow-terraform/main.tf
+++ mlflow-terraform/main.tf
@@ -3,11 +3,7 @@
 provider "aws" {
   region = "eu-central-1"
 }
-// Mlflow bucket will get a random unique name
-resource "aws_s3_bucket_acl" "mlflow_bucket_acl" {
-  acl = "private"
-  bucket = aws_s3_bucket.mlflow_bucket.id
-}
+
 resource "aws_s3_bucket" "mlflow_bucket" {
   tags = {
     Name = "Mlflow model artifacts bucket"
